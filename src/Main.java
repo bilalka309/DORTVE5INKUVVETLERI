616337582946b07673d9ea04ff02717e241f8b13
bilal karahan <bilalkarahan309@gmail.com>
2022-09-15 15:15:45 +0300

    sa

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner inp=new Scanner(System.in);
+        System.out.println("Sayınızı giriniz:");
+        int i=1;
+        int n = inp.nextInt();
+        System.out.println("4'ün kuvvetleri:");
+        while(i<=n){
+            System.out.print(i+",");
+            i*=4;
+        }
+        System.out.println("");
+        System.out.println("5'in kuvvetleri");
+        i=1;
+        while (i<=n){
+            System.out.print(i+",");
+            i*=5;
+        }
+
+
+
     }
 }
